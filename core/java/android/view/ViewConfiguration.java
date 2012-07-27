602e849ccce726fc6b3ff16117e51f702f0dd3c2
Phil Tunstall <ptunstall@gmail.com>
2012-07-27 12:00:44 +0100

    Hardware key custom rebinding (1/2)
    Framework changes to allow rebinding of the actions performed on the following
    key press events: Home long-press, menu press, menu long-press, search press,
    search long-press, app-switch press and app-switch long-press.
    
    The available actions are: Nothing, open/close menu, recent apps switcher,
    search assistant, voice search and in-app search.
    
    Patch Set 1: Initial port from ICS [done by Konstantin Koslowski]
    Patch Set 2: Added 'assist' (search) key customisation
                 Option for ICS-style in-app search, in addition to jellybean-style
                 search assistant.
                 Fixed double haptic feedback on virtual key press.
    Patch Set 3: Checkbox to enable/disable all custom bindings.
                 Home long-press defaults to no action on devices with
                 an app-switch key.
    Patch Set 4: The recent apps list will be preloaded on the initial down press
                 of whichever key to which it is bound.
    Patch Set 5: Fixed menu virtual key press sometimes performing the custom
                 action bound to the menu key instead of just opening or closing
                 the menu as it should.
    
    Change-Id: I20a67d46fd90a71d0ae9a9e4170a163ab8d8f6d5

--- core/java/android/view/ViewConfiguration.java
+++ core/java/android/view/ViewConfiguration.java
@@ -224,6 +224,8 @@ public class ViewConfiguration {
     private boolean sHasPermanentMenuKeySet;
     private Context mContext;
 
+    private Context mContext;
+
     static final SparseArray<ViewConfiguration> sConfigurations =
             new SparseArray<ViewConfiguration>(2);
 
@@ -682,11 +684,13 @@ public class ViewConfiguration {
     public boolean hasPermanentMenuKey() {
         // The action overflow button within app UI can
         // be controlled with a system setting
-        boolean showOverflowButton = Settings.System.getBoolean(
+        int showOverflowButton = Settings.System.getInt(
                 mContext.getContentResolver(),
-                Settings.System.UI_FORCE_OVERFLOW_BUTTON, false);
-        if (!showOverflowButton) {
-            return true;
+                Settings.System.UI_FORCE_OVERFLOW_BUTTON, 0);
+        if (showOverflowButton == 1) {
+            // Force overflow button on by reporting that
+            // the device has no permanent menu key
+            return false;
         } else {
             return sHasPermanentMenuKey;
         }
