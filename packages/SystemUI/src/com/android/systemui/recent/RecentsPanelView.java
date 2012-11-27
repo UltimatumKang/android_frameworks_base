30721ac6f1b44a0b93ce1cda282916bd0528f28b
Stevespear426 <stephen.k.spear@gmail.com>
2012-11-27 16:41:46 +0100

    Make RamBAr Moar Dynamicer...
    The listener code was already in running state just implementing it
    here as practice for the ram tile im gonna make.
    
    Signed-off-by: Stevespear426 <stephen.k.spear@gmail.com>

--- packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
+++ packages/SystemUI/src/com/android/systemui/recent/RecentsPanelView.java
@@ -72,7 +72,7 @@ import android.text.format.Formatter;
 import java.util.ArrayList;
 
 public class RecentsPanelView extends FrameLayout implements OnItemClickListener, RecentsCallback,
-        StatusBarPanel, Animator.AnimatorListener {
+        StatusBarPanel, Animator.AnimatorListener, RunningState.OnRefreshUiListener {
     static final String TAG = "RecentsPanelView";
     static final boolean DEBUG = TabletStatusBar.DEBUG || PhoneStatusBar.DEBUG || false;
     private PopupMenu mPopup;
@@ -399,10 +399,12 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
     }
 
     public void dismiss() {
+        mState.pause();
         ((RecentsActivity) mContext).dismissAndGoHome();
     }
 
     public void dismissAndGoBack() {
+        mState.pause();
         ((RecentsActivity) mContext).dismissAndGoBack();
     }
 
@@ -469,7 +471,7 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
     @Override
     protected void onFinishInflate() {
         super.onFinishInflate();
-
+        mState = RunningState.getInstance(getContext());
         mRecentsContainer = (ViewGroup) findViewById(R.id.recents_container);
         mStatusBarTouchProxy = (StatusBarTouchProxy) findViewById(R.id.status_bar_touch_proxy);
         mListAdapter = new TaskDescriptionAdapter(mContext);
@@ -521,6 +523,7 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
                 ((BitmapDrawable) mRecentsScrim.getBackground()).setTileModeY(TileMode.REPEAT);
             }
         }
+        mState.resume(this);
         UpdateRamBar();
     }
 
@@ -688,7 +691,6 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
         if (mRecentsActivity.isActivityShowing()) {
             refreshViews();
         }
-      UpdateRamBar();
     }
 
     private void updateUiElements() {
@@ -709,7 +711,6 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
                 R.plurals.status_bar_accessibility_recent_apps, numRecentApps, numRecentApps);
         }
         setContentDescription(recentAppsAccessibilityDescription);
-        UpdateRamBar();
     }
 
     public boolean simulateClick(int persistentTaskId) {
@@ -770,7 +771,6 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
         if (usingDrawingCache) {
             holder.thumbnailViewImage.setDrawingCacheEnabled(false);
         }
-        UpdateRamBar();
     }
 
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -808,7 +808,6 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
             sendAccessibilityEvent(AccessibilityEvent.TYPE_VIEW_SELECTED);
             setContentDescription(null);
         }
-        UpdateRamBar();
     }
 
     private void startApplicationDetailsActivity(String packageName) {
@@ -890,9 +889,24 @@ public class RecentsPanelView extends FrameLayout implements OnItemClickListener
         dismissAndGoBack();
     }
 
+    @Override
+    public void onRefreshUi(int what) {
+        switch (what) {
+            case REFRESH_TIME:
+                UpdateRamBar();
+                break;
+            case REFRESH_DATA:
+                UpdateRamBar();
+                break;
+            case REFRESH_STRUCTURE:
+                UpdateRamBar();
+                break;
+        }
+    }
+
     private void UpdateRamBar(){
         mAm = (ActivityManager)getContext().getSystemService(Context.ACTIVITY_SERVICE);
-        mState = RunningState.getInstance(getContext());
+        //mState = RunningState.getInstance(getContext());
 
         boolean recent_kill_all_button = Settings.System.getBoolean(mContext.getContentResolver(),
                       Settings.System.RECENT_KILL_ALL_BUTTON, false);
