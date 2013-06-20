23315dcfb5f592b2014e97f3973f19c5b6046b34
winner00 <toverturf@gmail.com>
2013-06-20 14:01:27 -0600

    Mass revert of commits that could cause problems. Revert "Adjust kernel data stack TCP parameters to optimize performance"
    This reverts commit f738aaa247f46251f06e038530b56dc7832fa48e.
    
    Revert "One more fix for incorrect indices."
    
    This reverts commit 3141fccc0a55367128cdb4708a2b042f7d1dc0cb.
    
    Revert "Fix array index oob in Matrix3f.loadRotate()."
    
    This reverts commit 35bd0680585833a3deb0e5c48b95f52e9814f1ee.
    
    Revert "tune up boot animation"
    
    This reverts commit 07166899a4a5819c7e0f7689144ff09fb7330d4d.
    
    Revert "fix potential memory leak"
    
    This reverts commit b716953f8a46abd02f6c04d1ec30ac51b738951a.
    
    Revert "Fixed a leaked partial wakelock in AbstractThreadedSyncAdapter."
    
    This reverts commit 84c49324c108ac036c7033a2267cf93890b6356c.
    
    Revert "Fix deadlock of WebViewCoreThread"
    
    This reverts commit 946aa4add7c9533dda3079f29ea7d621b28a0328.
    
    Revert "Fix graphical artifact in the fisheye effect"
    
    This reverts commit e3a583751097726cb19a9ab8f2dc323ba6a765ad.
    
    Revert "Rethrow an exception with stack trace information."
    
    This reverts commit 0e4bc236002df6390305f5772e05247e7bef0520.
    
    Revert "Applying ChannelScrolling to remove calculateDragAngle"
    
    This reverts commit 8b999697399f664944fc25f53060df5ab8b2a915.
    
    Revert "KeyChain: return null instead of throw"
    
    This reverts commit 9fce93e944c2a2d60aa7a44cefe518f78ac244fe.
    
    Revert "StreamingZipInflater: fix mmap'd end of read"
    
    This reverts commit fe4d93ce13800065ea7a916235ace9de522e7714.
    
    Revert "BootReceiver: Skip subdirectories inside tombstones scan"
    
    This reverts commit ae7b1b030a0c07085b9a2ac97a4e52186e332387.
    
    Revert "remove some lag launching apps and improve scrolling"
    
    This reverts commit 85c2a6d4eb27378548b243afdfad108f12a37d77.
    
    Revert "Fix a programmatic smooth scroll bug"
    
    This reverts commit 59b525cc21d6c270267ff6fb8b4ea3822c7b132c.
    
    Revert "Add additional Method that check whether a table is empty or not"
    
    This reverts commit 7e1152249b420ae5ef2139249272a77479415071.
    
    Revert "Properly rotate full [-pi, +pi] orientation range if available"
    
    This reverts commit fcf420fabc9ff42eee4365c3d5bfde952e077e22.
    
    Revert "remove leak WaitResult List in startActivityMayWait"
    
    This reverts commit 56fdadaacc77e6066f3edcfed7164788d663ecc0.
    
    Revert "framework:base: iterator error in singletone provider"
    
    This reverts commit 5e5a37e4e29199e3347eff25baaa73da0913316c.
    
    Revert "Framework/base: Added synchronization block to avoid race condition"
    
    This reverts commit 2db45d948c4bfce3a48854e090396109c9cbfd83.

--- services/java/com/android/server/am/ActivityManagerService.java
+++ services/java/com/android/server/am/ActivityManagerService.java
@@ -6350,7 +6350,6 @@ public final class ActivityManagerService extends ActivityManagerNative
                     // it runs in the process of the default user.  Get rid of it.
                     providers.remove(i);
                     N--;
-                    i--;
                     continue;
                 }
 
--- services/java/com/android/server/am/ActivityStack.java
+++ services/java/com/android/server/am/ActivityStack.java
@@ -3271,7 +3271,6 @@ final class ActivityStack {
                         } catch (InterruptedException e) {
                         }
                     } while (!outResult.timeout && outResult.who == null);
-                    mWaitingActivityLaunched.remove(outResult);
                 } else if (res == ActivityManager.START_TASK_TO_FRONT) {
                     ActivityRecord r = this.topRunningActivityLocked(null);
                     if (r.nowVisible) {
@@ -3288,7 +3287,6 @@ final class ActivityStack {
                             } catch (InterruptedException e) {
                             }
                         } while (!outResult.timeout && outResult.who == null);
-                        mWaitingActivityVisible.remove(outResult);
                     }
                 }
             }
@@ -3704,9 +3702,7 @@ final class ActivityStack {
         }
 
         if (activityRemoved) {
-            synchronized (mService) {
-                resumeTopActivityLocked(null);
-            }
+            resumeTopActivityLocked(null);
         }
 
         return res;
