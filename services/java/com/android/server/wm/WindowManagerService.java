464ef5c8364634e3bf9e712483851e78ed11cd95
Daniel MM <daxx4182@gmail.com>
2013-07-18 10:19:52 +0800

    Revert "Detect whether overflow button should be shown based on menu key state"
    This reverts commit e64abe4684998a4dceae037914485848ce15a64f.

--- services/java/com/android/server/wm/WindowManagerService.java
+++ services/java/com/android/server/wm/WindowManagerService.java
@@ -10478,11 +10478,6 @@ public class WindowManagerService extends IWindowManager.Stub
         return mPolicy.hasNavigationBar();
     }
 
-    @Override
-    public boolean hasMenuKeyEnabled() {
-        return mPolicy.hasMenuKeyEnabled();
-    }
-
     public void lockNow(Bundle options) {
         mPolicy.lockNow(options);
     }
