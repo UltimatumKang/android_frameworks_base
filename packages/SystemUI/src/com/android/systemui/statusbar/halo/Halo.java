c807d799e5b509364ea19c35331ae7680168dc46
Daniel MM <daxx4182@gmail.com>
2013-07-21 13:47:50 +0800

    HALO: Tablet Mode is unsupported for now.

--- packages/SystemUI/src/com/android/systemui/statusbar/halo/Halo.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/halo/Halo.java
@@ -442,11 +442,7 @@ public class Halo extends FrameLayout implements Ticker.TickerCallback {
 
     public void setStatusBar(BaseStatusBar bar) {
         mBar = bar;
-        if(ExtendedPropertiesUtils.isTablet()) {
-            if (mBar.getTabletTicker() != null) mBar.getTabletTicker().setUpdateEvent(this);
-        } else {
-            if (mBar.getTicker() != null) mBar.getTicker().setUpdateEvent(this);
-        }
+        if (mBar.getTicker() != null) mBar.getTicker().setUpdateEvent(this);
         mNotificationData = mBar.getNotificationData();
         loadLastNotification(true);
     }
